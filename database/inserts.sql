5a8ec54d2c198e5020cdd6a8e7a31c802f680886
Black Code <pabloandrescriolloalvarez@gmail.com>
2024-01-30 01:55:33 -0500

    Configuracion

--- database/inserts.sql
+++ database/inserts.sql
@@ -3,3 +3,9 @@ INSERT INTO cliente (nombre, apellido, fechaNacimiento, direccionDomicilio, celu
 INSERT INTO cliente (nombre, apellido, fechaNacimiento, direccionDomicilio, celular) VALUES ('Pablo Andres', 'Criollo Alvarez', '2004-12-07', 'San Jose', '0980520307');
 
 -- Poblar Cuenta
+INSERT INTO cuenta (idCliente, numeroCuenta, saldoCuenta) VALUES ((SELECT idCliente FROM cliente WHERE nombre = 'Pablo Andres' AND apellido = 'Criollo Alvarez'),'2583641579',1500.00);
+INSERT INTO cuenta (idCliente, numeroCuenta, saldoCuenta) VALUES ((SELECT idCliente FROM cliente WHERE nombre = 'Augusto Valentino' AND apellido = 'Davila Robles'),'2583641579',854.28);
+
+-- Poblar Tarjeta
+INSERT INTO tarjeta (idTarjeta, idCliente, marcaTarjeta, numeroTarjeta, fechaEmision, fechaExpiracion, codigoCVV, pin) VALUES ((SELECT idCliente FROM cliente WHERE nombre = 'Pablo Andres' AND apellido = 'Criollo Alvarez'),'VISA', '4567891238456159', '2022-08-17', '2027-08-17', '752', '7193');
+INSERT INTO tarjeta (idTarjeta, idCliente, marcaTarjeta, numeroTarjeta, fechaEmision, fechaExpiracion, codigoCVV, pin) VALUES ((SELECT idCliente FROM cliente WHERE nombre = 'Augusto Valentino' AND apellido = 'Davila Robles'),'VISA', '36975348619742659', '2021-06-21', '2028-06-21', '964', '4613');
